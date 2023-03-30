bfcf687e44373b2ece0426108a59f43e295878a4
Arian <Efimov15.96@mail.ru>
2023-03-30 13:18:25 +0900

    validation

--- src/js/main.js
+++ src/js/main.js
@@ -49,4 +49,54 @@ $(document).ready(function () {
       $(".catalog-item__list").eq(i).toggleClass("catalog-item__list--active");
     });
   });
+
+  // Модальное окно
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn();
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay , #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button--mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn();
+    });
+  });
+
+  // Валидация форм
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 3,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Введите имя",
+          minlength: jQuery.validator.format("Введите {0} символов"),
+        },
+        phone: "Введите +7 формате",
+        email: {
+          required: "Введите почту",
+          email: "Указжите корректную почту",
+        },
+      },
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
 });
