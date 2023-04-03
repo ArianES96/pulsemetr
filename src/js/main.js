de31e5f6db8b3f4b33b4e3661b180ac73800d186
Arian <Efimov15.96@mail.ru>
2023-04-03 22:06:23 +0900

    finsh

--- src/js/main.js
+++ src/js/main.js
@@ -99,4 +99,28 @@ $(document).ready(function () {
   valideForms("#order form");
 
   $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  // Скроолл на сайте, показывает стрелку вверх при оапределенном уровне
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
 });
